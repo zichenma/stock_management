f03c20ba4df66d431c5eeefabde6553a3ef8a625
Liepin-NorthAmerica <nini10171017>
2019-02-26 00:08:24 -0500

    finished 2_6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+    StockManageComponent,
   ],
   imports: [
     BrowserModule,
